a6db05c5e0ebf6f1aff5ae3029162ec7d9f91275
Rodri1996 <69223756+Rodri1996@users.noreply.github.com>
2021-12-14 17:09:50 -0300

    Se agrega el archivo de rutas con algunas de ellas para ir a las pantallas de Login y Busqueda de Rutinas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -16,7 +18,8 @@ import { LoginComponent } from './components/login/login.component';
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
