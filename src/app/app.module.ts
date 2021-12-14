2a323a785fa12990cc5b34b66faea6f9894d55da
Rodri1996 <69223756+Rodri1996@users.noreply.github.com>
2021-12-14 13:08:38 -0300

    Se agrega el componente footer con sus estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
