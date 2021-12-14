3a36d3de00c4ec9df7f7bb95f503c5c39deea951
Rodri1996 <69223756+Rodri1996@users.noreply.github.com>
2021-12-14 16:53:17 -0300

    Se avanza con la pantalla de Login agregandole los campos de Username y Password, y estilos css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SearchRoutinesComponent } from './components/search-routines/search-routines.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     FooterComponent,
-    SearchRoutinesComponent
+    SearchRoutinesComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
